f4b01df333d940851be7a727393cde032c39f6a2
Jeremy Lunsford <45763644+mukn@users.noreply.github.com>
2024-09-10 15:35:57 -0400

    Update Get-ScryfallSet.ps1
    Updated to better capture the dataset. Needs some error reporting.

--- Get-ScryfallSet.ps1
+++ Get-ScryfallSet.ps1
@@ -1,20 +1,37 @@
-# Input set code and define static params
-$set = "dsk"
-$uri = "https://api.scryfall.com/"
-$req = "sets/"
+function Get-ScryfallSet {
+  # Variable parameters
+  param(
+    [Parameter(Mandatory=$true)]
+    [string[]] $Set
+    )
+  
+  # Define static params
+  $uri = "https://api.scryfall.com/"
+  $req = "sets/"
+  $cardList
+  $paginated = $false
+  
+  # Build URI and make request
+  $requestUri = $uri + $req + $set
+  
+  # Get resulting json
+  $result = Invoke-WebRequest -Uri $requestUri | ConvertFrom-Json
+  
+  # Isolate search URI
+  $searchUri = $result.search_uri
 
-# Build URI and make request
-$requestUri = $uri + $req + $set
+  # Build card data
+  $cardData = Invoke-WebRequest -Uri $searchUri | ConvertFrom-Json
 
-# Get resulting json
-$result = Invoke-WebRequest -Uri $requestUri | ConvertFrom-Json
+  # Extract card list
+  $cardList += $cardData.data
 
-# Isolate search URI
-$searchUri = $result.search_uri
+  # Start paginated data
+  while ($cardData.has_more) {
+    $searchUri = $cardData.next_page
+    $cardData = Invoke-WebRequest -Uri $searchUri | ConvertFrom-Json
+    $cardList += $cardData.data
+  }
 
-# Build card data
-$cardData = Invoke-WebRequest -Uri $searchUri | ConvertFrom-Json
-
-# Extract card list
-$cardList = $cardData.data
-
+  return $cardList
+}
